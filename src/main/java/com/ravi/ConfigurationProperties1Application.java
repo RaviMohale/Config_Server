093f2df66d006c31dda0e6a455deb397c9c3b2b0
RDevloper <ravimohale0190@gmail.com>
2023-06-18 12:12:43 +0530

    first commit

---
+++ src/main/java/com/ravi/ConfigurationProperties1Application.java
@@ -0,0 +1,13 @@
+package com.ravi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationProperties1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationProperties1Application.class, args);
+	}
+
+}
